ad2503b4b5ce731e9ba2eb9d9e5f8fb7ee502e1a
John <jrued1227@gmail.com>
2018-10-03 19:05:13 -0400

    added  express

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const fs = require("fs");
 const discord = require("discord.js");
 const CSVtoArray = require("csv-array");
 
+const express = require("express");
+const app = express();
+
 const auth = require("./auth.json");
 const START = "S1";
 const END = "E2";
+
+// Run express server for OpenShift
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, ip, () =>
+    console.log(`Example app listening on port ${port}!`)
+);
 
 var client = new discord.Client();
 
